1d1f09ab09cb9fbb72e5ca0c15c49386ec9ddef4
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-04-01 23:30:48 +0530

    Ganesh Adding UC2 for Address Book File

--- Program.cs
+++ Program.cs
@@ -11,6 +11,35 @@ namespace AddressBook_LINQ
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Address Book LINQ");
+            //Create Object for DataTable
+            ContactDataManager contactDataManager = new ContactDataManager();
+            ContactDataManager contactDataManagers = new ContactDataManager();
+            DataTableManager dataTableManger = new DataTableManager();
+            dataTableManger.CreateDataTable();
+
+            //Insert Values into Table
+            contactDataManager.FirstName = "Saguna";
+            contactDataManager.LastName = "Ukade";
+            contactDataManager.PhoneNumber = 9977668754;
+            contactDataManager.Email = "saguna98@gmail.com";
+            contactDataManager.Address = "503,B Block,Shivane";
+            contactDataManager.City = "Pune";
+            contactDataManager.State = "Maharashtra";
+            contactDataManager.zip = 411023;
+            dataTableManger.InsertintoDataTable(contactDataManager);
+
+            //Insert Values into Table
+            contactDataManagers.FirstName = "Amruta";
+            contactDataManagers.LastName = "Sharma";
+            contactDataManagers.PhoneNumber = 8867543212;
+            contactDataManagers.Email = "amruta23@gmail.com";
+            contactDataManagers.Address = "78 street,Kolhapur";
+            contactDataManagers.City = "Kolhapur";
+            contactDataManagers.State = "Maharashtra";
+            contactDataManagers.zip = 4321981;
+            dataTableManger.InsertintoDataTable(contactDataManagers);
+            dataTableManger.Display();
+
         }
     }
 }
